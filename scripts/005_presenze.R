0096b4a3ae5b0872d689dbac86774dabf93d1357
Marco Chiapello <chiapello.m@gmail.com>
2022-10-12 10:40:31 +0200

    20221011

--- scripts/005_presenze.R
+++ scripts/005_presenze.R
@@ -10,8 +10,8 @@ players <- readr::read_table("data/003_dati/players.tsv")
 tail(pres, n = 14)
 
 pres <- add(players,
-    date = "20221007", ### RICORDARSI DI CAMBIARE DATA ##
-    assenti = c(5, 0))
+    date = "20221011", ### RICORDARSI DI CAMBIARE DATA ##
+    assenti = c(0))
 
 tail(as.data.frame(pres), n = 24)
 
